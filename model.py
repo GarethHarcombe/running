22f175ecb52b6e2cb114e1bb71e61407eecdba87
Gareth Harcombe <gharcombe2001@outlook.com>
2022-03-03 21:41:43 +1300

    feat: basic model training

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+        self.linear_relu_stack = nn.Sequential(            
+            nn.Linear(60 * 60 * 6 * 4, 60 * 60),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.Linear(60 * 60, 60),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Linear(60, 3),
         )
 
     def forward(self, x):
--- session_detect.py
+++ session_detect.py
@@ -1,9 +1,11 @@
 import numpy as np
 import torch
 import math
-import fitdecode
-from gps_loader import GPSDataset
+from gps_loader import get_dataloaders
 from torch.utils.data import DataLoader
+from torch import nn
+from model import NeuralNetwork
+from tqdm import tqdm
 
 
 def haversine_dist(a, b):
@@ -21,12 +23,58 @@ def haversine_dist(a, b):
     return d
 
 
+def train_loop(dataloader, model, loss_fn, optimizer):
+    model.train()
+    size = len(dataloader.dataset)
+    for sample in tqdm(iter(dataloader)):
+        # Compute prediction and loss
+        pred = model(sample["data"])
+        loss = loss_fn(pred, sample["type"])
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        #if batch % 100 == 0:
+            #loss, current = loss.item(), batch * len(X)
+            #print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for sample in tqdm(iter(dataloader)):
+            pred = model(sample["data"])
+            test_loss += loss_fn(pred, sample["type"]).item()
+            correct += (pred == sample["type"]).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
 def main():
-    gps_dataset = GPSDataset("/home/gareth/Documents/Programming/running/export_12264640/activities.csv",
-                         "/home/gareth/Documents/Programming/running/export_12264640/")    
-    train_dataloader = DataLoader(gps_dataset, batch_size=64, shuffle=True)
+    model = NeuralNetwork()
     
+    dataloaders = get_dataloaders("/home/gareth/Documents/Programming/running/export_12264640/", "activities.csv")
+    train_dataloader = dataloaders["Train"]
+    test_dataloader = dataloaders["Test"]
     
+    learning_rate = 1e-3
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    
+    epochs = 10
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+    print("Done!")    
     
 
 
